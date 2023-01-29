ae0e66e892b559321728639e3c33d779e5d7ae85
Naveen Bandara <s92067119@ousl.lk>
2023-01-29 18:05:05 +0530

    Fuel stations Add
    Added fuel stations on the map and details updated

--- screen/fuelstations/fuel.js
+++ screen/fuelstations/fuel.js
@@ -3,19 +3,47 @@ import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Text} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-paper';
+import Stations from './stations';
+import * as Location from 'expo-location'
 
 export default function Fuel(){
     const navigation=useNavigation()
+    const[mapRegion,setmapRegion]=useState({
+
+    })
+
+    const userLocation = async()=>{
+        let {status}= await Location.requestForegroundPermissionsAsync();
+        if(status!=='granted'){
+            setErrorMessage('Permission to access location was denied');
+            return;
+        }
+        let location = await Location.getCurrentPositionAsync({timeInterval:1000});
+        setmapRegion(location);
+        console.log(location.coords.latitude,location.coords.longitude)
+    }
+    useEffect(()=>{
+        userLocation();
+    },[]);
 return(   
         <View style={styles.container}>
             <View style={styles.navigater}>
                 <Button onPress={()=>navigation.goBack()}>Go back</Button>
             </View>     
             <View style={styles.mapcontain}>
-                <MapView style={styles.map}>
-                    <Marker>
+                <MapView style={styles.map} region={mapRegion}>
+                {Stations.map((station) => (
+                    <Marker
+                        key={station.name}
+                        coordinate={{
+                            latitude: station.latitude,
+                            longitude: station.longitude
+                        }}
+                        title={station.name}
+                        description={station.address}
+                    />
+                ))}
                         
-                    </Marker>
                     
                 </MapView>
             </View>    
